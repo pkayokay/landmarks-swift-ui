ce4febf76d61775a916b62a0ae9f1088e2cad2ef
Paul Kim <paulk@hey.com>
2021-04-25 16:36:58 -0400

    Create favoriate button inside landmark detail view

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by paulkim on 4/25/21.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? Color.yellow : Color.gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
