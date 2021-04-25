98e94b06bdd09d96dda89ca640bd24956c3641eb
Paul Kim <paulk@hey.com>
2021-04-25 16:31:20 -0400

    Adopt model objects in views

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
-
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
     var filteredLandmarks: [Landmark] {
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
@@ -37,5 +37,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
     }
 }
